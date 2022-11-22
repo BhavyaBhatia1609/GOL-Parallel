2db34e05520770d425253e1feda85f6223b235f5
Yousuf Alawadhi <78476924+yousufala@users.noreply.github.com>
2022-11-22 15:58:57 +0000

    singleThread

--- gol/distributor.go
+++ gol/distributor.go
@@ -58,7 +58,7 @@ var server *string
 
 func init() {
 	//serverIP := "localhost:8030"
-	serverIP := "44.212.21.187:8030"
+	serverIP := "54.210.25.81:8030"
 	server = flag.String("server", serverIP, "IP:port string to connect to as server")
 }
 
